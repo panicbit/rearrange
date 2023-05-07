482ec86bff254fff827b120980ea5dc28e325c18
panicbit <panicbit.dev@gmail.com>
2023-05-07 14:48:47 +0200

    inline rearrange function

--- src/slice.rs
+++ src/slice.rs
@@ -8,24 +8,16 @@ pub trait Rearrange<T> {
 }
 
 impl<T> Rearrange<T> for &mut [T] {
-    fn rearrange<F>(self, f: F)
+    fn rearrange<F>(self, mut f: F)
     where
         T: Ord,
         F: for<'x> FnMut(Order<'x, T>) -> Order<'x, T>,
     {
-        rearrange(self, f)
+        self.sort_by(|a, b| {
+            let order = Order::new(a, b);
+            let order = f(order);
+
+            order.ordering()
+        });
     }
-}
-
-fn rearrange<T, F>(arr: &mut [T], mut f: F)
-where
-    T: Ord,
-    F: for<'x> FnMut(Order<'x, T>) -> Order<'x, T>,
-{
-    arr.sort_by(|a, b| {
-        let order = Order::new(a, b);
-        let order = f(order);
-
-        order.ordering()
-    });
 }
